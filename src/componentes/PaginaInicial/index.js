90c9c605b9e0131a1ef9597c789fd11acecf0e21
Felipe Silva <felipe.dacs@gmail.com>
2020-08-08 18:05:11 -0300

    :tv: add aula 1

---
+++ src/componentes/PaginaInicial/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './estilo.css';
+
+export default function PaginaInicial() {
+  const [ numeroAleatorio, setNumeroAleatorio ] = useState(1);
+
+  return(
+    <div className="conteudo-centralizado">
+      <h3>Número aleatório:</h3>
+      <h1>{ numeroAleatorio }</h1>
+
+      <div className='area-botao'>
+        <label>
+          Click no botão abaixo para gerar um número aleatório:
+        </label>
+
+        <button>
+          Gerar número
+        </button>
+      </div>
+    </div>
+  );
+}
